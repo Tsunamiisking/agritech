2fb0ca63b23ad079ad28dd52257e918bd66af051
Allen Douglas <allenissabigboi@gmail.com>
2024-10-25 16:11:52 -0700

    Added images and fixed tailwind

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section>
+        <div>
+            <img>
+            </img>
+        </div>
+
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Get-Started</li>
+            </ul>
+
+        </div>
+    </section>
+
+  )
+}
+
+export default Header
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className='font-playfair'>
       Hello World
     </div>
   )
