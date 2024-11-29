1ac10fdb7aea650b74fb227ca45cce68a4c9cfae
Allen Douglas <allenissabigboi@gmail.com>
2024-11-29 13:58:07 -0800

    implementing Loading logic

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <img src="/images/loading.gif" alt="Loading..." className="w-16 h-16" />
+  </div>
+  )
+}
+
+export default Loading
