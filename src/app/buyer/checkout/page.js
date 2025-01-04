6b3b552d47986dde82cd3b9bd98737821fd340e6
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-01-04 23:07:35 +0100

    pages route

---
+++ src/app/buyer/checkout/page.js
@@ -0,0 +1,13 @@
+import SideNav from "@/components/SideNav";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <SideNav />
+      Checkout
+    </div>
+  );
+}
+
+export default page;
