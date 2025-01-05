b3a27af76dcd463664f06694d5356f755ca0b0f5
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-01-05 21:11:05 +0100

    created component for related products

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -5,6 +5,7 @@ import { BsCartPlus } from "react-icons/bs";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { IoCallOutline } from "react-icons/io5";
 import { useRouter } from "next/navigation";
+import RelatedProducts from "./RelatedProducts";
 
 function Modal({
   product,
@@ -70,37 +71,7 @@ function Modal({
           </div>
         </div>
         {/* Related Products Section */}
-        {filteredProducts && (
-          <div className="hidden md:block md:w-10/12">
-            <h3 className="text-xl font-semibold text-white text-center mb-4">
-              More Products in {product.category}
-            </h3>
-            <div className="grid grid-cols-3 gap-2 h-96 overflow-y-scroll">
-              {filteredProducts.map((prod) => (
-                <div
-                  key={prod.id}
-                  className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center h-56"
-                >
-                  <Image
-                    alt="product Image"
-                    src={prod.imageUrl}
-                    width={150}
-                    height={100}
-                    className="rounded-md h-24"
-                  />
-                  <p className="mt-2 text-sm font-semibold">{prod.name}</p>
-                  <p className="text-gray-500 text-sm">#{prod.price}</p>
-                  <button
-                    className="mt-4 px-3 py-1 rounded-lg bg-primary text-white hover:bg-secondary transition duration-300"
-                    onClick={() => openModal(prod)}
-                  >
-                    More
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
+       <RelatedProducts filteredProducts={filteredProducts}/>
       </div>
     </div>
   );
