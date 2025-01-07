868a3786498bfd11ef9ddd54c0cbf65a906ebb23
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-01-07 20:52:50 +0100

    properly passsed product props

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import Modal from "./Modal";
+import Modal from "./ProductCard";
 import products from "../data/products";
 
 function Product() {
--- src/components/RelatedProducts.js
+++ src/components/RelatedProducts.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-function RelatedProducts({filteredProducts}) {
+function RelatedProducts({filteredProducts, product}) {
   return (
     <div>
           {filteredProducts && (
