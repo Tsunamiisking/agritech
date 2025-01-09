6c54adb2b31025e358905fa23e972332d83c3b75
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-01-09 20:59:17 +0100

    configured persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+  reducer: {
+    cart: persistedReducer,
+  },
 });
 
+export  const persistor =  persistStore(store);
 export default store;
