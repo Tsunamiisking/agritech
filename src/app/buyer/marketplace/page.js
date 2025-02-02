c44269f166fef2c996c7e012f840ffa4f9619be9
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-02-02 23:37:52 +0100

    removed redundant code

--- src/app/buyer/marketplace/page.js
+++ src/app/buyer/marketplace/page.js
@@ -1,25 +1,14 @@
 "use client";
 import Marketplace from "@/components/Marketplace";
 import SideNav from "@/components/SideNav";
-import React, { useEffect, useState } from "react";
 import ProtectedRoute from "@/components/ProtectedRoutes";
-import { CustomerType } from "@/utils/CustomerType";
-import { useRouter } from "next/navigation";
-import Loading from "@/components/Loading";
 
 
 const Page = () => {
   return (
     <>
       <div className="flex px-5 lg:px-0">
-        <SideNav
-          userType="Buyer"
-          option1="MarketPlace"
-          option2="Order History"
-          option3="Cart"
-          option4="Favourite"
-        />
-
+        <SideNav />
         <Marketplace />
       </div>
     </>
