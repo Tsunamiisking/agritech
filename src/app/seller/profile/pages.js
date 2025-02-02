73ea45156300c7e9f14caa869fd9545aa8752b27
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-02-02 23:37:25 +0100

    created profile page for both buyer and seller

--- src/app/seller/profile/pages.js
+++ src/app/seller/profile/pages.js
@@ -1,17 +1,21 @@
+import SideNavSeller from '@/components/SideNavSeller'
 import React from 'react'
 
 function pages() {
   return (
-    <div>profile & Settings
+    <>
+    <SideNavSeller/>
+    <div className='lg:ml-64 md:ml-64'>profile & Settings
         {/* todo
         Store Details: Add or update store name, location, and contact details. Upload store logo or banner.
-	•	Policies: Add return, refund, and shipping policies.
+        •	Policies: Add return, refund, and shipping policies.
 	•	Account Settings: Update personal or store profile information.
-        Help and Support
+  Help and Support
 	•	Access to FAQs and guides for troubleshooting.
 	•	Contact support for technical or operational issues.
 	•	Live chat or ticketing system for resolving disputes. */}
     </div>
+  </>
   )
 }
 
