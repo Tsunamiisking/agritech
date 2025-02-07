8d466fd79888730714ecf3fda20bfd7732044357
Allen Douglas <allenissabigboi@gmail.com>
2025-02-06 20:57:35 -0800

    feat: integrate Layout and SideNav components into order management and payment management pages for improved structure

--- src/app/seller/paymentmanagement/page.js
+++ src/app/seller/paymentmanagement/page.js
@@ -1,13 +1,24 @@
+import Layout from "@/components/Layout";
+import SideNav from "@/components/SideNav";
 import React from "react";
 
 function page() {
   return (
-    <div>
-      Payment Management
-      {/* Summary of earnings and payouts.
+    <Layout>
+      <SideNav
+        option1={"Dashboard"}
+        option2={"Product management"}
+        option3={"Order Management"}
+        option4={"Payment Management"}
+        userType={"Seller"}
+      />
+      <div>
+        Payment Management
+        {/* Summary of earnings and payouts.
 	•	Withdrawal options (e.g., mobile money, bank transfer).
 	•	Transaction history for tracking payments and fees. */}
-    </div>
+      </div>
+    </Layout>
   );
 }
 
