ce477ca2d502c2dbb609404364f09feea42516b0
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-02-10 22:43:39 +0100

    created product.M page layout

--- src/app/seller/productmanagement/page.js
+++ src/app/seller/productmanagement/page.js
@@ -4,6 +4,7 @@ import UploadForm from "@/components/UploadForm";
 import ProductListing from "@/components/ProductListing"; // Ensure this is imported
 import React, { useState } from "react";
 import Layout from "@/components/Layout";
+import ProductManagement from "@/components/ProductManagement";
 
 function Page() {
   const [activeComponent, setActiveComponent] = useState("uploadForm"); // Default component
@@ -17,38 +18,7 @@ function Page() {
         option4={"Payment Management"}
         userType={"Seller"}
       />
-      <div className="md:ml-64 lg:ml-64">
-        <div>
-
-          {/* todo
-          add product
-          product listing: list of uploaded product
-          edit update or deleteproduct
-            */}
-          <button
-            onClick={() => setActiveComponent("uploadForm")}
-            className={`${
-              activeComponent === "uploadForm" ? "bg-blue-500 text-white" : "bg-gray-200 text-black"
-            } px-4 py-2 rounded`}
-          >
-            Add Product
-          </button>
-          <button
-            onClick={() => setActiveComponent("productListing")}
-            className={`${
-              activeComponent === "productListing" ? "bg-blue-500 text-white" : "bg-gray-200 text-black"
-            } px-4 py-2 rounded ml-2`}
-          >
-            Product Listing
-          </button>
-        </div>
-
-        {/* Dynamically Render Components */}
-        <div className="mt-4">
-          {activeComponent === "uploadForm" && <UploadForm />}
-          {activeComponent === "productListing" && <ProductListing />}
-        </div>
-      </div>
+      <ProductManagement/>
     </Layout>
   );
 }
